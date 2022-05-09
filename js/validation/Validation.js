a162563b950bbf5d6e7222c19dbabc485ae7f6bb
AisteB <aistebuneviciene@gmail.com>
2022-05-09 05:18:00 +0200

    Vardo, pavardes, email testai

---
+++ js/validation/Validation.js
@@ -0,0 +1,64 @@
+class Validation {
+    isValidFirstName(firstName) {
+        if (firstName === undefined ||
+            typeof firstName !== 'string'||
+            firstName.length < 2 ||
+            !this.isUpperCase(firstName[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidLastName(lastName) {
+        if (lastName === undefined ||
+            typeof lastName !== 'string'||
+            lastName.length < 2 ||
+            !this.isUpperCase(lastName[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidEmail() {
+        return true;
+    }
+
+    isValidMessage() {
+        return true;
+    }
+
+    isValidPhoneNumber() {
+        return true;
+    }
+
+    isValidMonthName() {
+        return true;
+    }
+
+    isValidMonthName() {
+        return true;
+    }
+
+    isValidWeekDayName() {
+        return true;
+    }
+
+    isValidWeekDayName() {
+        return true;
+    }
+
+    isValidPersonID() {
+        return true;
+    }
+
+    isUpperCase(letter) {
+        // if(letter === letter.toUpperCase()) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+
+        return letter === letter.toUpperCase();
+    }
+}
+
